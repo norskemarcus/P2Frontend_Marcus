1f332e4f76014088d0405ed0e018dfbffd7aa0b9
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2023-05-02 12:48:33 +0200

    P2-1 added modal with info

--- pages/users-modal/users-modal.js
+++ pages/users-modal/users-modal.js
@@ -1,57 +1,74 @@
-const URL = "https://jsonplaceholder.typicode.com/users/"
-import { sanitizeStringWithTableRows } from "../../utils.js"
-
+const URL = "https://jsonplaceholder.typicode.com/users/";
+import { sanitizeStringWithTableRows } from "../../utils.js";
 
 export function initUsersModal() {
-  document.getElementById("tbl-body").onclick = showUserDetails
-  getAllUsers()
+  document.getElementById("tbl-body").onclick = showUserDetails;
+  getAllUsers();
 }
 
 export async function getAllUsers() {
   try {
-    const usersFromServer = await fetch(URL).then(res => res.json())
-    showAllData(usersFromServer)
-  }
-  catch (err) {
-    console.error("UPPPPPS: " + err) //This can be done better - do it
+    const usersFromServer = await fetch(URL).then((res) => res.json());
+    showAllData(usersFromServer);
+  } catch (err) {
+    console.error("UPPPPPS: " + err); //This can be done better - do it
   }
 }
 
 function showAllData(data) {
-  const tableRowsArray = data.map(user => `
+  const tableRowsArray = data.map(
+    (user) => `
   <div class="card m-3" style="width:400px">
   <div class="card-body">
     <h4 class="card-title">${user.name}</h4>
     <p class="card-text">${user.address.street}.</p>
-    <a href="#" class="btn btn-primary">See Profile</a>
+    <button id="row-btn_details_${user.id}" type="button" class="btn btn-sm btn-primary" 
+    data-bs-toggle="modal"
+    data-bs-target="#exampleModal">Details</button> 
+    
   </div>
   </div>
 
-  `)
+  `
+  );
 
-
-  const tableRowsString = tableRowsArray.join("\n")
-  document.getElementById("tbl-body").innerHTML = sanitizeStringWithTableRows(tableRowsString)
-
-  
+  const tableRowsString = tableRowsArray.join("\n");
+  document.getElementById("tbl-body").innerHTML =
+    sanitizeStringWithTableRows(tableRowsString);
 }
 
 async function showUserDetails(evt) {
-  const target = evt.target
+  const target = evt.target;
   if (!target.id.startsWith("row-btn_")) {
-    return
+    return;
   }
-  
-  const parts = target.id.split("_");
-  const id = parts[2]
-  const btnAction = parts[1]
-    if (btnAction === "details") {
-      alert("Here you can Add an option to view details for user with id: " + id )
-      
-    } 
-    else 
-    if (btnAction === "delete")  {
-        alert("Here you can DELETE user with id: " + id )
-    }
-    
+  const parts = target.id.split("_"); // laver et array og splitter ved _
+  // row-btn = 0, details/delete = 1 og id = 2
+  const id = parts[2];
+  const btnAction = parts[1];
+  if (btnAction === "details") {
+    // bootstrap 5 modal
+    document.querySelector("#exampleModalLabel").innerText =
+      "Information om sovepose " + id;
+
+    const user = await fetch(URL + id)
+      .then((res) => res.json())
+      .then((user) => {
+        document.querySelector("#modal-body").innerText = `
+        Mærke: ${user.name}
+        Produktnavn: ${user.name}
+        Pris: 
+        Længde:
+        Komforttemp.(°C):
+        Laveste temp. (°C):
+        Fyld:
+        Vægt (g):
+        Farve: 
+        Sæson:
+        Sortiment:
+        Varenr:
+
+        `;
+      });
+  }
 }
